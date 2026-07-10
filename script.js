136cd753099c1e3fc6051ca4ee48191363f6b8c2
MaheshMergu01 <maheshmergu14@gmail.com>
2026-07-10 20:11:38 +0530

    Fixing Food music

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ function gameEngine() {
 
   // If you have eaten the food, then Increment the score & regenerate the food
   if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
+    foodSound.play();
     snakeArr.unshift({
       x: snakeArr[0].x + inputDir.x,
       y: snakeArr[0].y + inputDir.y
