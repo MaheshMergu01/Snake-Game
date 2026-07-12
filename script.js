064b5dd7366cab0f2d2ce8bc23e72c661d6e0ff9
MaheshMergu01 <maheshmergu14@gmail.com>
2026-07-12 20:08:58 +0530

    Updating high Score

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ food = { x: 6, y: 7 };
 // Game Functions
 function main(ctime) {
   window.requestAnimationFrame(main);
-// console.log(ctime);
+  // console.log(ctime);
   if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
     return;
   }
@@ -25,16 +25,20 @@ function main(ctime) {
 function isCollide(snake) {
   // If you have bumb into yourself
   for (let i = 1; i < snakeArr.length; i++) {
-    if(snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
       return true;
     }
   }
 
   // If you bumb into the wall
-    if (snake[0].x >= 18 || snake[0].x <= 0 || snake[0].y >= 18 || snake[0].y <= 0) {
-      return true;
-    }
-  
+  if (
+    snake[0].x >= 18 ||
+    snake[0].x <= 0 ||
+    snake[0].y >= 18 ||
+    snake[0].y <= 0
+  ) {
+    return true;
+  }
 }
 function gameEngine() {
   // Part 1: Updating the Snake Array & Food
@@ -52,7 +56,12 @@ function gameEngine() {
   if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
     foodSound.play();
     score += 1;
-    scoreBox.innerHTML = 'Score: ' + score;
+    if (score > highscoreval) {
+      highscoreval = score;
+      localStorage.setItem("highscore", JSON.stringify(highscoreval));
+      highscoreBox.innerHTML = "High Score: " + highscoreval;
+    }
+    scoreBox.innerHTML = "Score: " + score;
     snakeArr.unshift({
       x: snakeArr[0].x + inputDir.x,
       y: snakeArr[0].y + inputDir.y
@@ -96,10 +105,18 @@ function gameEngine() {
   board.appendChild(foodElement);
 }
 // Main Logics
+let highscore = localStorage.getItem("highscore");
+if (highscore === null) {
+  highscoreval = 0;
+  localStorage.setItem("highscore", JSON.stringify(highscoreval));
+} else {
+  highscoreval = JSON.parse(highscore);
+  highscoreBox.innerHTML = "High Score: " + highscore;
+}
 window.requestAnimationFrame(main);
 window.addEventListener("keydown", e => {
-  inputDir = { x: 0, y: 1 }; 
-// Start the Game
+  inputDir = { x: 0, y: 1 };
+  // Start the Game
   moveSound.play();
   switch (e.key) {
     case "ArrowUp":
